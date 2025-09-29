24b09489c115c8daca733f98da770326ac40064f
ashwanising011 <ashwanisingh9737@gmail.com>
2025-09-29 17:29:28 +0530

    Added more controllers

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -128,8 +128,8 @@ const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndDelete(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
